396c121cbfdc2f587bbfc052de01087a0006c479
전범준 <gagl142857@gmail.com>
2026-05-19 15:05:40 +0900

    '배포'

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+app.set('trust proxy', 1);
 const 포트 = process.env.PORT || 5000;
 
 const limiter = rateLimit({
